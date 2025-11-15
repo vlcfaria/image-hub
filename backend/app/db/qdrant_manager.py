61fa2f8d602dc72b9f32cdea7ecf8f7d67930d8b
Vinicius Faria <artixsproductions@gmail.com>
2025-11-15 20:19:27 -0300

    feat: added retry condition for waiting qdrant service to start

--- backend/app/db/qdrant_manager.py
+++ backend/app/db/qdrant_manager.py
@@ -1,5 +1,16 @@
 import logging
 from qdrant_client import QdrantClient, models
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(3))
+def _wait_for_qdrant(client):
+    try:
+        client.info()
+        logging.info("Qdrant is online.")
+    except Exception as e:
+        logging.warning(f"Waiting for Qdrant... ({e})")
+        raise
+
 
 class QdrantManager:
     client: QdrantClient = None
@@ -7,6 +18,7 @@ class QdrantManager:
     async def connect_to_database(self, path: str):
         logging.info("Connecting to MongoDB.")
         self.client = QdrantClient(url=path)
+        _wait_for_qdrant(self.client)
         #Idempotent call to create an index in the id field
         self.client.create_payload_index(
             collection_name='image_hub',
